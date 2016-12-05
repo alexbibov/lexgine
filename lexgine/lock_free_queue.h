303929f4e01c2fcca2e26f991fc43c4bba7a584a
Alex Bibov <lexer42152@gmail.com>
2016-12-05 21:17:52 +0200

    Implement hazard pointers

--- lexgine/lock_free_queue.h
+++ lexgine/lock_free_queue.h
@@ -18,14 +18,41 @@ private:
     //! Encapsulates pointer together with access counter
     struct NodePointer
     {
-        Node* ptr;    //!< actual pointer to the following node
-        uint32_t counter;    //!< number of times the pointer has been updated after creation (needed to circumvent the ABA-problem)
+        std::atomic<Node*> ptr;    //!< actual pointer to the following node
+        std::atomic<int32_t>* count;    //!< reference counter for the pointer. This is needed to avoid the ABA and the memory reclamation problems. The special value of -1 means that the pointer was already removed
+
+        NodePointer()
+        {
+            std::atomic_init(&ptr, NULL);
+
+
+        }
+
+        NodePointer(Node* ptr)
+        {
+            std::atomic_init(&this->ptr, ptr);
+
+
+        }
+
+        NodePointer(NodePointer const& other)
+        {
+            Node* other_pointer = other.ptr.load(memory_order_acquire);
+            ptr.store(&other_pointer, memory_order_release);
+
+            int32_t other_pointer_count = other.count.load(memory_order_acquire);
+        }
+
+        ~NodePointer()
+        {
+
+        }
     };
 
     struct Node
     {
         T value;    //!< actual value of the node
-        std::atomic<NodePointer> next;    //!< "combined pointer" to the following node
+        NodePointer next;    //!< "combined pointer" to the following node
     };
 
 
@@ -34,12 +61,7 @@ public:
 
     LockFreeQueue()
     {
-        Node* dummy_node = new Node{};
-        std::atomic_init(&dummy_node->next, NodePointer{ nullptr, 0U });
 
-        std::atomic_init(&m_head, NodePointer{ dummy_node, 0U });
-        std::atomic_init(&m_tail, NodePointer{ dummy_node, 0U });
-        std::atomic_init(&m_queue_element_counter, 0U);
     }
 
     ~LockFreeQueue()
@@ -50,43 +72,7 @@ public:
     //! adds new node with the given value into the queue
     void enqueue(T const& value)
     {
-        Node* p_node = new Node{};
-        p_node->value = value;
-        std::atomic_init(&p_node->next, NodePointer{ nullptr, 0U });
 
-        NodePointer volatile tail_estimate;
-        NodePointer volatile next_estimate;
-        while (true)
-        {
-            tail_estimate = m_tail.load();
-            next_estimate = tail_estimate.ptr->next.load();
-
-            // Check if the loaded tail corresponds to the current tail value
-            NodePointer volatile aux = m_tail.load();
-            if (std::memcmp(&tail_estimate, &aux, sizeof(NodePointer)) == 0)
-            {
-                // Is the "next" node actually a NULL-pointer?
-                if (next_estimate.ptr == nullptr)
-                {
-                    // Attempt to link the new node to the queue
-                    NodePointer new_node_pointer{ p_node, next_estimate.counter + 1 };
-                    if(tail_estimate.ptr->next.compare_exchange_weak(&next_estimate, &new_node_pointer))
-                        break;
-                }
-                else
-                {
-                    // Tail does not point to the last node in the queue. Attempt to advance it.
-                    NodePointer new_tail_pointer{ next_estimate.ptr, tail_estimate.counter + 1 };
-                    m_tail.compare_exchange_weak(&tail_estimate, &new_tail_pointer);
-                }
-            }
-        }
-
-        // Set the tail to point to the newly inserted node
-        NodePointer new_tail_value{ p_node, tail_estimate.count + 1 };
-        m_tail.compare_exchange_weak(&tail_estimate, &new_tail_value);
-
-        ++m_queue_element_counter;
     }
 
 
@@ -95,38 +81,7 @@ public:
     */
     misc::Optional<T> dequeue()
     {
-        NodePointer volatile head_estimate;
-        NodePointer volatile tail_estimate;
-        NodePointer volatile next_estimate;
-        while (true)
-        {
-            head_estimate = m_head.load();
-            tail_estimate = m_tail.load();
-            next_estimate = head_estimate.ptr->next.load();
 
-            // Check if the head value is consistent
-            NodePointer volatile aux = m_head.load();
-            if (std::memcmp(&head_estimate, &aux, sizeof(NodePointer)) == 0)
-            {
-                if (head_estimate.ptr == tail_estimate.ptr)    // the queue is either empty or its tail is falling behind
-                {
-                    if (next_estimate.ptr == nullptr)
-                    {
-                        // the queue is empty, return invalid value
-                        return misc::Optional<T>{};
-                    }
-                    // attempt to advance the tail
-                    NodePointer new_tail_value{ next_estimate.ptr, tail_estimate.counter + 1 };
-                    m_tail.compare_exchange_weak(&tail_estimate, &new_tail_value);
-                }
-                else
-                {
-                    T dequeued_value = next_estimate.ptr->value;
-                }
-            }
-        }
-
-        --m_queue_element_counter;
     }
 
 
