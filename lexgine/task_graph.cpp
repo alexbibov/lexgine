40d855397c0e8d4833d7e5c15883685d2a343a05
Alex Bibov <aleksandr.bibov@futuremark.com>
2017-02-10 16:50:30 +0200

    Begin implementation of the task graph

--- lexgine/task_graph.cpp
+++ lexgine/task_graph.cpp
@@ -13,6 +13,10 @@ uint8_t lexgine::core::concurrency::TaskGraph::getNumberOfWorkerThreads() const
     return m_num_workers;
 }
 
+void lexgine::core::concurrency::TaskGraph::execute()
+{
+}
+
 void TaskGraph::createDotRepresentation(std::string const& destination_path) const
 {
     std::string dot_graph_representation;
