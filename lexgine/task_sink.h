e9d6fd7765033389d367d479ead2fd943992babc
Alex Bibov <lexer42152@gmail.com>
2017-02-16 08:30:20 +0200

    Complete implementation of the task sink

--- lexgine/task_sink.h
+++ lexgine/task_sink.h
@@ -15,9 +15,14 @@ public:
     void run();    //! runs the task sink
 
 private:
+	void dispatch(uint8_t worker_id);    //!< function looped by worker threads
 
+	std::list<std::unique_ptr<std::thread>> m_workers_list;    //!< list of work threads
+	
     RingBufferTaskQueue m_task_queue;    //!< concurrent task queue
     std::list<AbstractTask*> const& m_task_list;    //!< reference to the list of tasks provided by the task graph
+
+	bool m_exit_signal;    //!< becomes 'true' when the sink encounters an exit task, which has been successfully executed
 };
 
 }}}
