176a8aad4d06167cbc0752d16c7f29ee49036ac1
Alex Bibov <lexer42152@gmail.com>
2017-07-12 10:22:58 +0300

    Add example compute PSO descriptor to the sample XML script

--- lexgine/core/dx/d3d12/d3d12_pso_xml_parser.h
+++ lexgine/core/dx/d3d12/d3d12_pso_xml_parser.h
@@ -40,7 +40,9 @@ public:
 
 
     /*! Constructs the parser and immediately parses provided sources
-     constructing related PSO description structures
+     constructing related PSO description structures. 
+     NOTE: parameter node_mask identifies, for which nodes to create PSO descriptors.
+     For example if node_mask=0x3, all descriptors will be dubbed for the 0-th node and for the 1-st node
     */
     D3D12PSOXMLParser(std::string const& xml_source, bool deferred_shader_compilation = true, uint32_t node_mask = 0x1);
 
@@ -82,6 +84,7 @@ private:
 
     std::string const m_source_xml;
     bool m_deferred_shader_compilation;
+    uint32_t m_node_mask;
     std::unique_ptr<impl> m_impl;
     std::list<GraphicsPSODescriptorCacheEntry> m_graphics_pso_descriptor_cache;
     std::list<ComputePSODescriptorCacheEntry> m_compute_pso_descriptor_cache;
--- lexgine/core/dx/d3d12/tasks/hlsl_compilation_task.h
+++ lexgine/core/dx/d3d12/tasks/hlsl_compilation_task.h
@@ -3,6 +3,7 @@
 #include "../../../concurrency/schedulable_task.h"
 #include "../../../shader_source_code_preprocessor.h"
 #include "../../../data_blob.h"
+#include "../pipeline_state.h"
 
 #include <list>
 
@@ -27,13 +28,16 @@ public:
 
     /*! Establishes a new shader compilation task. The type of source is determined from parameters source and source_type.
      when source_type = file then source is interpreted as path to a file containing HLSL code to compile. If source_type = string then
-     source is interpreted as a string containing HLSL source code
+     source is interpreted as a string containing HLSL source code.
+     
+     Here p_target_pso_descriptors should point to a contiguous array of either graphics or compute descriptors that will receive 
+     compiled shader blob. The exact type of PSO descriptor as well as its shader stage receiving the blob is determined based on
+     provided value of parameter shader_type.
     */
     HLSLCompilationTask(std::string const& source, std::string const& source_name,
         ShaderType shader_type, std::string const& shader_entry_point, ShaderSourceCodePreprocessor::SourceType source_type,
+        void* p_target_pso_descriptors, uint32_t num_descriptors,
         std::list<HLSLMacroDefinition> const& macro_definitions = std::list<HLSLMacroDefinition>{});
-
-    D3DDataBlob getTaskData() const;
 
 private:
     bool do_task(uint8_t worker_id, uint16_t frame_index) override;    //! performs actual compilation of the shader
@@ -44,9 +48,9 @@ private:
     ShaderType m_type;
     std::string m_shader_entry_point;
     ShaderSourceCodePreprocessor::SourceType m_source_type;
+    void* m_p_target_pso_descriptors;
+    uint32_t m_num_target_descriptors;
     std::list<HLSLMacroDefinition> m_preprocessor_macro_definitions;
-
-    D3DDataBlob m_shader_bytecode;
 };
 
 }}}}}
