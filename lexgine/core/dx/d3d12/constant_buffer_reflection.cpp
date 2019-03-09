ec54a8b26d57d5f3e716f07ce8a72f941b36310d
Alex Bibov <lexer42152@gmail.com>
2019-03-09 15:28:47 +0200

    Fix packing rules in constant buffer reflection

--- lexgine/core/dx/d3d12/constant_buffer_reflection.cpp
+++ lexgine/core/dx/d3d12/constant_buffer_reflection.cpp
@@ -10,24 +10,25 @@ using namespace lexgine::core::dx::d3d12;
 bool ConstantBufferReflection::addElement(std::string const& name, ReflectionEntryDataType const& element_type)
 {
     HashedString hash{ name };
-    size_t data_size;
+    size_t element_size;
+
+    size_t next_16byte_boundary = (m_current_offset & (~0xFui64)) + 0x10ui64;
 
     if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float1)
         && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool1))
     {
-        data_size = 4 * element_type.array_elements_count;
+        element_size = element_type.array_elements_count > 1 ? 16 : 4;
     }
     else if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float2)
         && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool2x4))
     {
-        m_current_offset += (8 - (m_current_offset % 8)) % 8;    // padding
-        data_size = 8 * element_type.array_elements_count;
+        element_size = element_type.array_elements_count > 1 ? 16 : 4;
     }
     else if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float3)
         && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool4x4))
     {
-        m_current_offset += (16 - (m_current_offset % 16)) % 16;    // padding
-        data_size = 16 * element_type.array_elements_count;
+        
+        element_size = element_type.array_elements_count > 1 ? 16 : 4;
     }
     else
     {
@@ -35,8 +36,12 @@ bool ConstantBufferReflection::addElement(std::string const& name, ReflectionEnt
             "reflection \"" + getStringName() + "\": the element has unknown data type");
     }
 
+    if (m_current_offset + element_size > next_16byte_boundary) m_current_offset = next_16byte_boundary;
+    size_t data_size = element_size * element_type.array_elements_count;
+
     ReflectionEntry entry{ m_current_offset, element_type, data_size };
     m_current_offset += data_size;
+
     return m_reflection_data.insert(std::make_pair(hash, entry)).second;
 }
 
