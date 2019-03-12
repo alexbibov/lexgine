c5c90a29c2a45ad50e3c9ac8aebf5e65d9cb10a1
Alex Bibov <lexer42152@gmail.com>
2019-03-12 09:16:19 +0200

    Implement constant buffer data mappers

--- lexgine/core/dx/d3d12/constant_buffer_reflection.cpp
+++ lexgine/core/dx/d3d12/constant_buffer_reflection.cpp
@@ -1,5 +1,6 @@
-#include "lexgine/core/exception.h"
+#include <cassert>
 
+#include "lexgine/core/exception.h"
 #include "constant_buffer_reflection.h"
 
 
@@ -7,40 +8,39 @@ using namespace lexgine::core::misc;
 using namespace lexgine::core::dx::d3d12;
 
 
-bool ConstantBufferReflection::addElement(std::string const& name, ReflectionEntryDataType const& element_type)
+std::pair<unsigned, unsigned> ConstantBufferReflection::getReflectionEntryBaseTypeDimensions(ReflectionEntryBaseType entry_base_type)
 {
-    HashedString hash{ name };
-    size_t element_size;
+    int type_idx = static_cast<int>(entry_base_type);
+    unsigned row_count = type_idx < 4 ? 1 : (type_idx - 4) / 16 + 2;
+    unsigned column_count = type_idx < 4 ? 1 : (type_idx - 4) / 4 % 4 + 1;
+    return std::make_pair(row_count, column_count);
+}
 
-    size_t next_16byte_boundary = (m_current_offset & (~0xFui64)) + 0x10ui64;
+bool ConstantBufferReflection::addElement(std::string const& name, ReflectionEntryDesc const& entry_descriptor)
+{
+    auto row_and_column_counts = getReflectionEntryBaseTypeDimensions(entry_descriptor.base_type);
 
-    if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float1)
-        && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool1))
+    assert(entry_descriptor.element_count > 0);
+    assert(row_and_column_counts.first > 0);
+    assert(row_and_column_counts.second > 0);
+
+    size_t next_16byte_boundary = m_current_offset + ((0x10 - (m_current_offset & 0xF)) & 0xF);
+    size_t start_address, data_size;
+    if (entry_descriptor.element_count > 1 || row_and_column_counts.first > 1)
     {
-        element_size = element_type.array_elements_count > 1 ? 16 : 4;
-    }
-    else if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float2)
-        && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool2x4))
-    {
-        element_size = element_type.array_elements_count > 1 ? 16 : 4;
-    }
-    else if (static_cast<int>(element_type.base_type) >= static_cast<int>(ReflectionEntryBaseType::float3)
-        && static_cast<int>(element_type.base_type) <= static_cast<int>(ReflectionEntryBaseType::bool4x4))
-    {
-        
-        element_size = element_type.array_elements_count > 1 ? 16 : 4;
+        start_address = next_16byte_boundary;
+        data_size = 16 * (row_and_column_counts.first*entry_descriptor.element_count - 1) + 4 * row_and_column_counts.second;
     }
     else
     {
-        LEXGINE_THROW_ERROR_FROM_NAMED_ENTITY(this, "unable to add element \"" + name + "\" into constant buffer "
-            "reflection \"" + getStringName() + "\": the element has unknown data type");
+        data_size = 4 * row_and_column_counts.second;
+        start_address = m_current_offset + data_size > next_16byte_boundary ? next_16byte_boundary : m_current_offset;
     }
 
-    if (m_current_offset + element_size > next_16byte_boundary) m_current_offset = next_16byte_boundary;
-    size_t data_size = element_size * element_type.array_elements_count;
+    HashedString hash{ name };
 
-    ReflectionEntry entry{ m_current_offset, element_type, data_size };
-    m_current_offset += data_size;
+    ReflectionEntry entry{ start_address, entry_descriptor, data_size };
+    m_current_offset = start_address + data_size;
 
     return m_reflection_data.insert(std::make_pair(hash, entry)).second;
 }
@@ -48,6 +48,11 @@ bool ConstantBufferReflection::addElement(std::string const& name, ReflectionEnt
 ConstantBufferReflection::ReflectionEntry const& ConstantBufferReflection::operator[](std::string const& name) const
 {
     HashedString hash{ name };
+    return (*this)[hash];
+}
+
+ConstantBufferReflection::ReflectionEntry const& ConstantBufferReflection::operator[](HashedString const& hash) const
+{
     auto p = m_reflection_data.find(hash);
     if (p != m_reflection_data.end())
     {
@@ -55,7 +60,7 @@ ConstantBufferReflection::ReflectionEntry const& ConstantBufferReflection::opera
     }
     else
     {
-        LEXGINE_THROW_ERROR_FROM_NAMED_ENTITY(this, "unable to locate entry \"" + name + "\" in constant "
-            "buffer reflection \"" + getStringName() + "\"");
+        LEXGINE_THROW_ERROR_FROM_NAMED_ENTITY(this, std::string{ "unable to locate entry \"" } 
+            + hash.string() + "\" in constant buffer reflection \"" + getStringName() + "\"");
     }
 }
