392201e1a5e4787b7c85dcc830a284b398e24f5e
Alex Bibov <lexer42152@gmail.com>
2019-06-20 16:24:35 +0300

    Refactor flags

--- lexgine/core/misc/flags.h
+++ lexgine/core/misc/flags.h
@@ -7,17 +7,17 @@
 
 namespace lexgine::core::misc {
 
-template<typename BaseFlagType>
+template<typename BaseFlagsType>
 using is_scoped_enum = std::integral_constant<bool, 
-    std::is_enum<BaseFlagType>::value && !std::is_convertible<BaseFlagType, int>::value>;
+    std::is_enum<BaseFlagsType>::value && !std::is_convertible<BaseFlagsType, int>::value>;
 
 //! Template class that allows to implement "enumeration" types that could be OR'ed just like normal flags
-template<typename BaseFlagType, typename BaseIntegralType = int>
+template<typename BaseFlagsType, typename CompatibleIntegralType = int>
 class Flags
 {
 public:
-    using base_values = BaseFlagType;    //!< base flag values to be OR'ed
-    using base_int_type = BaseIntegralType;    //!< base integral type used to store flag values
+    using base_values = BaseFlagsType;    //!< base flag values to be OR'ed
+    using int_type = CompatibleIntegralType;    //!< base integral type used to store flag values
 
     //! Default initialization
     Flags() : m_value{ 0 }
@@ -26,19 +26,19 @@ public:
     }
 
     //! Initialization by value
-    Flags(BaseIntegralType value) : m_value{ value }
+    Flags(CompatibleIntegralType value) : m_value{ value }
     {
 
     }
 
     //! Initialization by a base flag value
-    Flags(BaseFlagType flag) : m_value{ static_cast<base_int_type>(flag) }
+    Flags(BaseFlagsType flag) : m_value{ static_cast<int_type>(flag) }
     {
 
     }
 
     //! Value assignment
-    Flags& operator =(BaseIntegralType value)
+    Flags& operator =(CompatibleIntegralType value)
     {
         m_value = value;
 
@@ -52,39 +52,39 @@ public:
     }
 
     //! Conversion to the base type (to enforce type safety only explicit variant is supported)
-    explicit operator BaseIntegralType() const
+    explicit operator CompatibleIntegralType() const
     {
         return m_value;
     }
 
     //! OR's this flag value with provided base flag value and returns the resulting flag value
-    Flags operator |(BaseFlagType const& base_flag) const
+    Flags operator |(BaseFlagsType const& base_flag) const
     {
-        return Flags{ m_value | static_cast<BaseIntegralType>(base_flag) };
+        return Flags{ m_value | static_cast<CompatibleIntegralType>(base_flag) };
     }
 
-    Flags& operator |=(BaseFlagType const& base_flag)
+    Flags& operator |=(BaseFlagsType const& base_flag)
     {
-        m_value |= static_cast<BaseIntegralType>(base_flag);
+        m_value |= static_cast<CompatibleIntegralType>(base_flag);
         return *this;
     }
 
     //! Sets given base flag bit. Equivalent to OR'ing this flag bit with the current value of the flags object.
     //! Returns reference to the updated flag object
-    Flags& set(BaseFlagType const& base_flag)
+    Flags& set(BaseFlagsType const& base_flag)
     {
-        m_value |= static_cast<BaseIntegralType>(base_flag);
+        m_value |= static_cast<CompatibleIntegralType>(base_flag);
         return *this;
     }
 
     //! Returns 'true' if requested base flag is set in this flag value. Returns 'false' otherwise
-    bool isSet(BaseFlagType const& base_flag) const
+    bool isSet(BaseFlagsType const& base_flag) const
     {
-        return (m_value & static_cast<BaseIntegralType>(base_flag)) == static_cast<BaseIntegralType>(base_flag);
+        return (m_value & static_cast<CompatibleIntegralType>(base_flag)) == static_cast<CompatibleIntegralType>(base_flag);
     }
 
     //! Returns encapsulated bitset
-    BaseIntegralType getValue() const
+    CompatibleIntegralType getValue() const
     {
         return m_value;
     }
@@ -97,7 +97,7 @@ public:
 
 
 private:
-    BaseIntegralType m_value;
+    CompatibleIntegralType m_value;
 };
 
 }
@@ -105,12 +105,12 @@ private:
 namespace lexgine 
 {
 
-template<typename BaseIntegralType,
-    typename T0 = std::enable_if<core::misc::is_scoped_enum<BaseIntegralType>::value>::type,
-    typename T1 = decltype(BaseIntegralType::_tag_flag_enumeration)>
-    core::misc::Flags<BaseIntegralType> operator | (BaseIntegralType a, BaseIntegralType b)
+template<typename BaseFlagsType,
+    typename T0 = std::enable_if<core::misc::is_scoped_enum<BaseFlagsType>::value>::type,
+    typename T1 = decltype(BaseFlagsType::_tag_flag_enumeration)>
+    core::misc::Flags<BaseFlagsType> operator | (BaseFlagsType a, BaseFlagsType b)
 {
-    return core::misc::Flags<BaseIntegralType>{a} | b;
+    return core::misc::Flags<BaseFlagsType>{a} | b;
 }
 
 }
