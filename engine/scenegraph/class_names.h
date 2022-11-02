8478bd0df967c3745f9e2558946b05caf57f0635
Alex Bibov <lexer42152@gmail.com>
2022-11-02 11:54:29 +0200

    Begin implementing scene loader
    Begin implementation of gltf scene loader and scetch the structure
    of scenegraph

---
+++ engine/scenegraph/class_names.h
@@ -0,0 +1,17 @@
+#ifndef LEXGINE_SCENEGRAPH_CLASS_NAMES_H
+#define LEXGINE_SCENEGRAPH_CLASS_NAMES_H
+
+namespace lexgine::scenegraph::class_names {
+
+extern char const Scene[];
+extern char const Camera[];
+extern char const Light[];
+extern char const Material[];
+extern char const Mesh[];
+extern char const Submesh[];
+extern char const Node[];
+extern char const Texture[];
+
+}
+
+#endif
