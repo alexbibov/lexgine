e7e2b90248f5fccce4052f1ed886dd1f480d4112
alexbibov <lexer42152@gmail.com>
2022-11-15 08:36:32 +0200

    Scetch the scenegraph
    — Scetch the basic structure of the main scenegraph objects
    — Implement light parser in the scene loader (based on KHR_lights_punctual extension)

--- engine/scenegraph/scene.cpp
+++ engine/scenegraph/scene.cpp
@@ -1,5 +1,6 @@
 
 #include <tinygltf/tiny_gltf.h>
+#include <3rd_party/glm/gtc/constants.hpp>
 #include "scene.h"
 
 namespace lexgine::scenegraph
@@ -26,9 +27,14 @@ std::shared_ptr<Scene> Scene::loadScene(std::filesystem::path const& path_to_sce
     Scene rv{};
 
 
+    if (!result) {
+        LEXGINE_LOG_ERROR(rv, "Unable to load gltf file '" + gltf_path_to_file + "'");
+        return nullptr;
+    }
     if (!error_buffer.empty())
     {
         LEXGINE_LOG_ERROR(rv, "Error loading gltf file '" + gltf_path_to_file + "': " + error_buffer);
+        return nullptr;
     }
 
     if (!warning_buffer.empty())
@@ -37,23 +43,154 @@ std::shared_ptr<Scene> Scene::loadScene(std::filesystem::path const& path_to_sce
     }
 
 
-    if (!result) {
-        LEXGINE_LOG_ERROR(rv, "Unable to load gltf file '" + gltf_path_to_file + "'");
-        return nullptr;
-    }
-
-
     rv.setStringName("gltf_scene_" + path_to_scene.stem().string());
 
     // Check used extensions
     for (auto& ext : gltf_model.extensionsUsed)
     {
-        if (auto it = rv.m_enabled_extensions.find(ext); it == rv.m_enabled_extensions.end())
+        auto it = rv.m_enabled_extensions.find(ext);
+        if (it == rv.m_enabled_extensions.end())
         {
-
+            if (std::find(gltf_model.extensionsRequired.begin(), gltf_model.extensionsRequired.end(), ext) != gltf_model.extensionsRequired.end())
+            {
+                LEXGINE_LOG_ERROR(rv, "Unable to load gltf file '" + gltf_path_to_file + "': required extension " + ext.c_str() + " is not supported");
+                return nullptr;
+            }
+            else {
+                core::misc::Log::retrieve()->out("gltf file '" + gltf_path_to_file + "' contains unsupported extension " + ext.c_str(), core::misc::LogMessageType::exclamation);
+            }
+        }
+        else
+        {
+            it->second = true;
         }
     }
+
+    if (!rv.loadLights(gltf_model)) return;
+    if (!rv.loadSamplers(gltf_model)) return;
+    if (!rv.loadImages(gltf_model)) return;
+    if (!rv.loadTextures(gltf_model)) return;
+    if (!rv.loadMaterials(gltf_model)) return;
+    if (!rv.loadMeshes(gltf_model)) return;
+    if (!rv.loadCameras(gltf_model)) return;
+    if (!rv.loadNodes(gltf_model)) return;
+    if (!rv.loadAnimations(gltf_model)) return;
+
+    //return rv;
 }
 
+bool Scene::loadLights(tinygltf::Model& model)
+{
+    if (m_enabled_extensions[c_khr_light_punctual_ext])
+    {
+        if (model.extensions.find(c_khr_light_punctual_ext) == model.extensions.end()
+            || !model.extensions.at(c_khr_light_punctual_ext).Has("lights"))
+        {
+            return false;
+        }
+
+        auto& khrLights = model.extensions.at(c_khr_light_punctual_ext).Get("lights");
+        m_lights.clear();
+        m_lights.reserve(khrLights.ArrayLen());
+        for (size_t i = 0; i < khrLights.ArrayLen(); ++i)
+        {
+            auto& light = khrLights.Get(static_cast<int>(i));
+
+            // Retrieve light type
+            if (!light.Has("type")) {
+                LEXGINE_LOG_ERROR(this, std::string{ c_khr_light_punctual_ext } + ": light " + std::to_string(i) + " does not have a type");
+                return false;
+            }
+
+            LightType lightType{};
+            {
+                std::string khrLightType = light.Get("type").Get<std::string>();
+                if (khrLightType == "directional")
+                {
+                    lightType = LightType::directional;
+                }
+                else if (khrLightType == "point")
+                {
+                    lightType = LightType::point;
+                }
+                else if (khrLightType == "spot")
+                {
+                    lightType = LightType::spot;
+                }
+                else
+                {
+                    LEXGINE_LOG_ERROR(this, std::string{ c_khr_light_punctual_ext } + ": light " + std::to_string(i) + " has invalid type");
+                    return false;
+                }
+            }
+
+            Light lexgineLight{ lightType };
+            lexgineLight.setStringName(light.Get("name").Get<std::string>());
+
+
+            // Retrieve light properties
+            if (light.Has("color"))
+            {
+                auto& light_color_property = light.Get("color");
+                glm::vec3 color{
+                    static_cast<float>(light_color_property.Get(0).Get<double>()),
+                    static_cast<float>(light_color_property.Get(1).Get<double>()),
+                    static_cast<float>(light_color_property.Get(2).Get<double>())
+                };
+                lexgineLight.setColor(color);
+            }
+
+            if (light.Has("intensity"))
+            {
+                lexgineLight.setIntensity(static_cast<float>(light.Get("intensity").Get<double>()));
+            }
+
+            if (lightType != LightType::point)
+            {
+                lexgineLight.setDirection({ 0.f, 0.f, -1.f });
+            }
+
+            switch (lightType)
+            {
+            case LightType::directional:
+                break;
+            case LightType::spot:
+            {
+                if (!light.Has("spot"))
+                {
+                    LEXGINE_LOG_ERROR(this, std::string{ c_khr_light_punctual_ext } + ": invalid description of spot light " + std::to_string(i) + ", the light does not define a 'spot' property, which is required");
+                    return false;
+                }
+
+                auto& light_spot_property = light.Get("spot");
+                lexgineLight.setInnerConeAngle(static_cast<float>(light_spot_property.Get("innerConeAngle").Get<double>()));
+
+                if (light_spot_property.Has("outerConeAngle"))
+                {
+                    lexgineLight.setOuterConeAngle(static_cast<float>(light_spot_property.Get("outerConeAngle").Get<double>()));
+                }
+                else
+                {
+                    lexgineLight.setOuterConeAngle(glm::pi<float>() / 4.f);
+                }
+            }
+
+            case LightType::point:
+                lexgineLight.setRange(static_cast<float>(light.Get("range").Get<double>()));
+                break;
+
+            }
+
+            m_lights.push_back(lexgineLight);
+        }
+    }
+
+    return true;
+}
+
+bool Scene::loadTextures(tinygltf::Model& model)
+{
+
+}
 
 }
