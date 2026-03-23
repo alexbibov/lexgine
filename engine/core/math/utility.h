ea3d7c23b11838542a9d5d309700065221a4ff1a
Alex Bibov <lexer42152@gmail.com>
2026-03-23 09:39:38 +0200

    Clean up utility code a bit

--- engine/core/math/utility.h
+++ engine/core/math/utility.h
@@ -9,7 +9,7 @@ namespace lexgine::core::math {
 extern double const pi;
 
 Matrix4f createProjectionMatrix(EngineApi target_api, float width, float height, 
-    float horizontal_fov = 120.f, float cutoff_distance = 10000.f, bool invert_depth = true);
+    float vertical_fov = 120.f, float cutoff_distance = 10000.f, bool invert_depth = true);
 
 Matrix4f createOrthogonalProjectionMatrix(EngineApi target_api, float position_x, float position_y,
     float widht, float height, float near_cutoff_distance = 1e-3f, float far_cutoff_distance = 1e4f);
